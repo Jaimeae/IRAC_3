bef60bae4dbc714090870cdc1bbae0f423153ee4
Jaime-am <46279145+Jaime-am@users.noreply.github.com>
2023-05-11 23:39:44 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ sudo mysql -u jaime <<-EOF
           create database irac; #crea DB para el login web
           use irac;
           CREATE TABLE usuarios (user VARCHAR(30) PRIMARY KEY UNIQUE,password VARCHAR(100),key VARCHAR(30));
-          INSERT INTO usuarios (user, password, key) VALUES ('user1', 'user1', 'INSERTAR');
-          INSERT INTO usuarios (user, password, key) VALUES ('user2', 'user2', 'INSERTAR');
+          INSERT INTO usuarios (user, password, key) VALUES ('test@test.es', 'test', 'INSERTAR');
+          INSERT INTO usuarios (user, password, key) VALUES ('test2@test.es', 'test2', 'INSERTAR');
 EOF
 wget https://www.bok.net/Bento4/binaries/Bento4-SDK-1-6-0-639.x86_64-unknown-linux.zip
 unzip Bento4-SDK-1-6-0-639.x86_64-unknown-linux.zip
